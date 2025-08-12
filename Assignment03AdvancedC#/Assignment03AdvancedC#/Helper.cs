ce6426cccd3dd98d197af3e6246a2db4ff47d63f
EmanMohamed36 <ei3377898@gmail.com>
2025-08-13 01:31:08 +0300

    Add assingment 3 in c#Advanced

---
+++ Assignment03AdvancedC#/Assignment03AdvancedC#/Helper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03AdvancedC_
+{
+    
+    internal class Helper
+    {
+        public static void ReverseQueue<T>(Queue<T> queue)
+        {
+            Stack<T> stack = new Stack<T>();
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+            while (stack.Count > 0)
+            {
+                queue.Enqueue(stack.Pop());
+            }
+        }
+    
+        public static void PrintQueue<T>(Queue<T> queue)
+        {
+            Console.WriteLine("Queue After Reverse");
+            foreach (var item in queue)
+            { 
+                Console.Write(item + " ");
+            }
+        }
+
+        static bool OpenParanthes(char c)
+        {
+            return c == '{' || c == '[' || c == '(';
+        }
+        public static bool IsBalanced(string input)
+        { 
+            Stack<char> paranthes = new Stack<char>();
+            int i;
+            for (i = 0; i < input.Length; i++)
+            {
+                if (OpenParanthes(input[i]))
+                {
+                    paranthes.Push(input[i]);
+                }
+                else
+                {
+                    if (paranthes.Count > 0)
+                    {
+                        char op = paranthes.Peek();
+                        if ((input[i] == '}' && op == '{') || (input[i] == ')' && op == '(') || (input[i] == ']' && op == '['))
+                        {
+                            if (paranthes.Count > 0)
+                                paranthes.Pop();
+                        }
+                        else break;
+
+                    }
+                    else break;
+                    
+                }
+            }
+              //  Console.WriteLine(i);
+            return(paranthes.Count == 0 && i == input.Length);
+        }
+    }
+}
---
+++ Assignment03AdvancedC#/Assignment03AdvancedC#/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Assignment03AdvancedC_
+{
+    internal class Program
+    {
+        static int ReadInt()
+        {
+            bool isInt = true;
+            int number;
+            do
+            {
+                if (!isInt)
+                { 
+                    Console.WriteLine("Enter Integer Value");
+                
+                }
+                isInt = int.TryParse(Console.ReadLine(), out number);
+            }
+            while (!isInt);
+            return number;
+        }
+        static bool IsParanthes(char c)
+        { 
+            return c == '{' || c == '}' || c == ']' || c == '[' || c == ')' || c == '(';
+        }
+        static string ReadStringOfParanthes()
+        {
+
+            bool isValid;
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                isValid = true;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (!IsParanthes(input[i]))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if (!isValid)
+                {
+                    Console.WriteLine("Please Enter correct String");
+                }
+
+            } while (!isValid);
+            return input;
+        }
+        static void Main(string[] args)
+        {
+            #region Q1
+            /*
+                 implement a function to reverse the elements of a queue using a stack.Given a Queue,
+             */
+            Queue<int> que = new Queue<int>();
+            Console.Write("Enter Count Queue: ");
+            int QSize = ReadInt();
+            Console.WriteLine("Enter Queue Elements: ");
+            for (int i = 0; i < QSize; i++)
+            { 
+                Console.Write($"Enter Element {i + 1}: ");
+                que.Enqueue(ReadInt());
+            }
+            
+
+            Helper.ReverseQueue(que);
+            Helper.PrintQueue(que);
+
+            #endregion
+
+            #region Q2
+            //Given a Stack, implement a function to check if a string of parentheses is balanced using a stack
+            Console.WriteLine();
+            Console.WriteLine("Enter String Of Patanthes");
+
+            string input = ReadStringOfParanthes();
+            bool isBalace = Helper.IsBalanced(input);
+            if(isBalace)
+            Console.WriteLine("Balanced");
+            else
+                Console.WriteLine("Not Balanced");
+            #endregion
+        }
+    }
+}
