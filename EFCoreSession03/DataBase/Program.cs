e06082b6646101a97af8f1a429919772230e609a
EmanMohamed36 <ei3377898@gmail.com>
2025-09-17 18:08:42 +0300

    EF_Core_Session03

---
+++ EFCoreSession03/DataBase/Program.cs
@@ -0,0 +1,46 @@
+﻿using DataBase.Models;
+
+namespace DataBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using NorthwindContext context = new NorthwindContext();
+            //Select 
+            //  int no = 5;
+            ////var res=  context.Categories.FromSqlRaw("select top ({0}) * from Categories",no);
+            //var res=  context.Categories.FromSqlInterpolated($"select top ({no}) * from Categories");
+
+            //  foreach (var item in res)
+            //  {
+            //      Console.WriteLine(item.CategoryName);
+            //  }
+
+            //DML
+
+            //int catId = 1;
+            //string newName = "Beverages";
+
+
+            ////context.Database.ExecuteSqlRaw("update Categories set CategoryName = {0} where CategoryId = {1}",newName,catId);
+            //context.Database.ExecuteSqlInterpolated($"update Categories set CategoryName = {newName} where CategoryId = {catId}");
+
+
+            NorthwindContextProcedures contextProcedures = new NorthwindContextProcedures(context);
+
+            //var res = contextProcedures.SelectAllCategoriesAsync().Result;
+            var res = contextProcedures.SalesByCategoryAsync("Beverages", "2018").Result;
+
+
+            foreach (var item in res)
+            {
+                Console.WriteLine($"{item.ProductName} ,, {item.TotalPurchase}");
+            }
+
+
+        }
+    }
+}
---
+++ EFCoreSession03/EFCoreSession03/Program.cs
@@ -0,0 +1,87 @@
+﻿using MappingInheritance.Data;
+using MappingInheritance.Data.Models;
+
+namespace MappingInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using MyCompanyContext myCompanyContext = new MyCompanyContext();
+
+            #region TPCT
+            //var emp01 = new FullTimeEmployee()
+            //{
+            //    Name="Rana",
+            //    Age=20,
+            //    Address="Mansoura",
+            //    Salary=3_000,
+            //    StartDate=DateTime.Now,
+            //};
+
+            //var emp02 = new PartTimeEmployee()
+            //{
+            //    Name = "Mai",
+            //    Age = 30,
+            //    Address = "Giza",
+            //    CountOfHrs=15,
+            //    HourRate=250
+            //};
+
+
+            //myCompanyContext.FullTimeEmployees.Add(emp01);
+            //myCompanyContext.PartTimeEmployees.Add(emp02);
+
+            //myCompanyContext.SaveChanges();
+
+
+
+            //var fullTimeEmp = myCompanyContext.FullTimeEmployees.FirstOrDefault();
+
+            //if(fullTimeEmp is not null)
+            //Console.WriteLine(fullTimeEmp.Name); 
+            #endregion
+
+            #region TPH
+
+            var emp01 = new FullTimeEmployee()
+            {
+                Name = "Mohamed",
+                Age = 20,
+                Address = "Ismailia",
+                Salary = 3_000,
+                StartDate = DateTime.Now,
+            };
+
+            var emp02 = new PartTimeEmployee()
+            {
+                Name = "Eman",
+                Age = 30,
+                Address = "Cairo",
+                CountOfHrs = 15,
+                HourRate = 250
+            };
+
+
+            myCompanyContext.Employees.Add(emp01);
+            myCompanyContext.Employees.Add(emp02);
+
+            myCompanyContext.SaveChanges();
+
+            //var res = myCompanyContext.Employees.ToList();
+            //if (res is not null)
+            //{
+
+            //    foreach (var item in res.OfType<FullTimeEmployee>())
+            //    {
+            //        Console.WriteLine($"{item.Name} ,,");
+            //    }
+
+
+            //   }
+
+
+            #endregion
+        }
+    }
+}
