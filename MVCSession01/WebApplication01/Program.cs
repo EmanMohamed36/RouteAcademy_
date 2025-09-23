fddde07f0eaa8ce35f4e46f958ff54d03a938ae7
EmanMohamed36 <ei3377898@gmail.com>
2025-09-23 23:36:49 +0300

    Add MVC Session 01 Demo

---
+++ MVCSession01/WebApplication01/Program.cs
@@ -0,0 +1,42 @@
+namespace WebApplication01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Services In DI Container
+            builder.Services.AddControllersWithViews();
+            #endregion
+
+            var app = builder.Build();
+
+            #region Pipleline
+
+            #endregion
+
+            //app.MapGet("/", () => "Hello World!"); //Default
+            //app.MapGet("/", () => "Hello World!"); -make error
+            app.MapGet("/eman", () => "Hello Eman!"); //static segment
+            app.MapGet("/{name}",async (context) =>
+            {
+                var name = context.GetRouteValue("name");
+                await context.Response.WriteAsync($"Hello {name}"); //dynamic segment
+            });
+
+            app.MapGet("/miss{name}", async (context) =>
+            {
+                var name = context.GetRouteValue("name");
+                await context.Response.WriteAsync($"Hello Miss {name}"); //mixed segment
+            });
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{Controller}/{Action}/{id?}",
+                defaults:  new {Controller =  "Movies" , Action = "Index"}
+                );
+            app.Run();
+        }
+    }
+}
---
+++ MVCSession01/WebApplication01/Properties/Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication01.Properties.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
